bce53f55502ee3010996eec7310e8a1b29a52557
Ananskikh_Timofei <408146@niuitmo.ru>
2024-11-23 04:30:33 +0300

    Aaaaaaaaaaaaaaaaaaaaaaaaa new everything changing filter

--- src/main/java/gramik/filters/UpdateFilters.java
+++ src/main/java/gramik/filters/UpdateFilters.java
@@ -10,7 +10,6 @@ public class UpdateFilters {
     public static final Predicate<Update> MESSAGE = x -> x.message() != null;
     public static final Predicate<Update> BUSINESS_MESSAGE = x -> x.businessMessage() != null;
     public static final Predicate<Update> TEXT_MESSAGE = MESSAGE.and(x -> x.message().text() != null);
-
     public static Predicate<Update> COMMAND(String command) {
         return TEXT_MESSAGE.and(x -> x.message().text().startsWith(command));
     }
